f0774986a58ecb35de1e9ced2e24254add5897a1
JadyAiCrypto <jdlee@aicrypto.ai>
2018-06-29 15:49:11 +0900

    update to test

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -10,12 +10,9 @@ contract TestToken is ReliableToken {
 
   uint256 public constant INITIAL_SUPPLY = 10000000000 * (10 ** uint256(decimals));
 
-  /**
-  * @dev Constructor that gives msg.sender all of existing tokens.
-  */
-  constructor() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  constructor(address initialAccount, uint initialBalance) public {
+    balances[initialAccount] = initialBalance;
+    totalSupply_ = initialBalance;
+    emit Transfer(0x0, initialAccount, initialBalance);
   }
 }
