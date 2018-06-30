0013ec1b144f15eb485e8d8258950e86acfedb35
JadyAiCrypto <jdlee@aicrypto.ai>
2018-06-30 13:34:59 +0900

    Improve calculation reliability

--- contracts/ReliableToken.sol
+++ contracts/ReliableToken.sol
@@ -19,11 +19,11 @@ contract ReliableToken is MintableToken, BurnableToken, Pausable, Lockable {
   /**
    * @dev Modifier to make a function callable only when the contract is not paused.
    */
-  modifier whenNotExceedLock(address _granted, uint256 _amount) {
+  modifier whenNotExceedLock(address _granted, uint256 _value) {
     uint256 lockedAmount = lockedAmountOf(_granted);
     uint256 balance = balanceOf(_granted);
 
-    require(balance > lockedAmount && balance.sub(lockedAmount) >= _amount);
+    require(balance > lockedAmount && balance.sub(lockedAmount) >= _value);
     _;
   }
 
